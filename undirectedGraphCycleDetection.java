edfa638c96a9e1e93c0005733981d42af9d550ea
Arjun Dubey <arjundjsce01@gmail.com>
2024-11-11 13:03:10 +0530

    Update undirectedGraphCycleDetection.java

--- undirectedGraphCycleDetection.java
+++ undirectedGraphCycleDetection.java
@@ -72,7 +72,7 @@ class Solution {
 class Solution {
     // Function to detect cycle in an undirected graph.
     
-    // BFS traversal Approach
+    // DFS traversal approach
     public boolean isCycle(ArrayList<ArrayList<Integer>> adj) {
         
         boolean [] visited = new boolean[adj.size()];
@@ -80,57 +80,27 @@ class Solution {
         
         for(int i=0; i<visited.length; i++){
             if(!visited[i]){
-                if(bfs(i, parent, adj, visited)) return true;
+                if(dfs(i, parent, adj, visited)) return true;
             }
         }
         
         return false;
-        
     }
     
-    boolean bfs(int node, int parent, ArrayList<ArrayList<Integer>> adj, boolean [] visited){
-        Queue<Pair> q = new LinkedList();
-        
-        q.add(new Pair(node, parent));
+    boolean dfs(int node, int parent, ArrayList<ArrayList<Integer>> adj, boolean [] visited){
         visited[node] = true;
         
-        while(!q.isEmpty()){
-            Pair pair = q.poll();
-            int child = pair.child();
-            int childParent = pair.parent();
-            
-            for(int adjNode : adj.get(child)){
-                if(!visited[adjNode]){
-                    q.add(new Pair(adjNode, child));
-                    visited[adjNode] = true;
-                }
-                else{
-                    if(adjNode != childParent){
-                        return true;
-                    }
+        for(int adjNode : adj.get(node)){
+            if(!visited[adjNode]){
+                if(dfs(adjNode, node, adj, visited)) return true;
+            }
+            else{
+                if(adjNode != parent){
+                    return true;
                 }
             }
         }
         
         return false;
     }
-    
-    class Pair{
-        int child;
-        int parent;
-        
-        Pair(int child, int parent){
-            this.child = child;
-            this.parent = parent;
-        }
-        
-        public int child(){
-            return this.child;
-        }
-        
-        public int parent(){
-            return this.parent;
-        }
-    }
 }
-
